5ae36db00e3fd1262096ab2651cc5bbbec35317f
Ajesh Pai <ajeshr2k1@gmail.com>
2021-05-13 12:14:40 +0530

    [+] Strings - Remove k consecutive

---
+++ strings/remove_k_consecutive_letters.py
@@ -0,0 +1,34 @@
+from queue import LifoQueue
+
+
+def remove_kth_consecutive(word, k):
+    stack = LifoQueue(len(word))
+
+    frequency = 1
+    for letter in word:
+
+        if stack.empty():
+            stack.put((letter, frequency))
+        else:
+            letter_at_top, frequency_of_letter = stack.get()
+            if letter_at_top == letter:
+                frequency_of_letter += 1
+                if frequency_of_letter<k:
+                    stack.put((letter, frequency_of_letter))
+            else:
+                stack.put((letter_at_top, frequency_of_letter))
+                stack.put((letter, frequency))
+
+    end_word = ''
+
+    while not stack.empty():
+        letter, frequency = stack.get()
+        if frequency <= k:
+            end_word = letter+end_word
+
+    return end_word
+
+if __name__ == '__main__':
+    word = "qddxxxd"
+    end_word = remove_kth_consecutive(word,3)
+    print(end_word)
