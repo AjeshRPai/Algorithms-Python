00392ca053fce59e666e7b9b961fef6085b847f8
Ajesh Pai <ajeshr2k1@gmail.com>
2021-05-21 19:29:19 +0530

    [+] Strings - Reversed Strings

---
+++ strings/reverse_words.py
@@ -0,0 +1,32 @@
+def reverseWordsInString(string):
+    words = []
+    start = 0
+
+    # store in the list untill space occurs
+    # then reverse the list
+    for index in range(len(string)):
+        character = string[index]
+
+        if character == " ":
+            print(string[start:index])
+            words.append(string[start:index])
+            start = index
+        elif string[start] == " ":
+            words.append(" ")
+            start = index
+    words.append(string[start:])
+    reverseList(words)
+    return "".join(words)
+
+
+def reverseList(list):
+    start, end = 0, len(list) - 1
+    while start < end:
+        list[start], list[end] = list[end], list[start],
+        start += 1
+        end -= 1
+
+
+if __name__ == '__main__':
+    word = "Algoexpert is the best"
+    reverseWordsInString(word)
