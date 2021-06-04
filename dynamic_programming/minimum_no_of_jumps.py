61e33592e656fa6bd5ef7383403d1ae2ca0a8a80
Ajesh Pai <ajeshr2k1@gmail.com>
2021-06-04 13:19:00 +0530

    [+] Dynamic Programming - Minimum Number of Jumps

---
+++ dynamic_programming/minimum_no_of_jumps.py
@@ -0,0 +1,19 @@
+def minNumberOfJumps(array):
+    if len(array) == 1:
+        return 0
+    jumps = 0
+    max_reach = array[0]
+    steps = array[0]
+
+    for i in range(1, len(array) - 1):
+        max_reach = max(max_reach, i + array[i])
+        steps -= 1
+        if steps == 0:
+            jumps += 1
+            steps = max_reach - i
+    return jumps + 1
+
+
+if __name__ == '__main__':
+    array = [3, 4, 2, 1, 2, 3, 7, 1, 1, 1, 3]
+    print(minNumberOfJumps(array))
