a5f437c7a3760ff2aba643321c70529fcd3c7851
Ajesh Pai <ajeshr2k1@gmail.com>
2021-06-14 10:22:01 +0530

    [+] Binary Search Tree : Iterative In order Traversal

---
+++ binary_trees/iterative_in_order_traversal.py
@@ -0,0 +1,25 @@
+def iterativeInOrderTraversal(tree, callback):
+    previous_node = None
+    current_node = tree
+
+    while current_node is not None:
+        if previous_node is None or previous_node == current_node.parent:
+            if current_node.left is not None:
+                next_node = current_node.left
+            else:
+                callback(current_node)
+                next_node = current_node.right if current_node.right is not None else current_node.parent
+        elif previous_node == current_node.left:
+            callback(current_node)
+            next_node = current_node.right if current_node.right is not None else current_node.parent
+        else:
+            next_node = current_node.parent
+        previous_node = current_node
+        current_node = next_node
+
+
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
