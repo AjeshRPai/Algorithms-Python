dcb50c9b6201099453fa3797361f12c980fdedb6
Ajesh R Pai <ajeshr2k1@gmail.com>
2024-09-05 14:02:29 +0530

    + Adds: reverse linked list

--- linkedList/reverse_linked_list.py
+++ linkedList/reverse_linked_list.py
@@ -1,76 +1,56 @@
-class LinkedList:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
+from typing import Optional
+# Define the ListNode class as per the initial implementation
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
-def reverse_linked_list(head):
-    prev = None
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        current = head
+        while current:
+            next_node = current.next  # Store the next node
+            current.next = prev  # Reverse the link
+            prev = current  # Move prev to current
+            current = next_node  # Move to the next node
+        return prev  # Prev will be the new head after the loop
+
+
+# Function to create a linked list from a list of values
+def create_linked_list(values):
+    if not values:
+        return None
+    head = ListNode(values[0])
     current = head
-
-    while current is not None:
-        next_node = current.next
-        current.next = prev
-        prev = current
-        current = next_node
-
-    head = prev
+    for value in values[1:]:
+        current.next = ListNode(value)
+        current = current.next
     return head
 
 
-# solution 2. By Using recursion
-def reverse_linked_list2(head: LinkedList):
-    print("head",head.value)
-    if head is None or head.next is None:
-        return head
-
-    print("head is ",head)
-    # Reverse the rest list
-    rest = reverse_linked_list2(head.next)
-
-    # Put first element at the end
-    head.next.next = head
-    head.next = None
-
-    # Fix the header pointer
-    return rest
+# Function to print the linked list
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.val, end=" -> " if current.next else "")
+        current = current.next
+    print()
 
 
-def print_linked_list(linkedList: LinkedList):
-    current_node = linkedList
+# Driver code
+if __name__ == "__main__":
+    # Create a linked list from a list of values
+    values = [1, 2, 3, 4, 5]  # Example list
+    head = create_linked_list(values)
 
-    list = []
-    while current_node.value is not None:
-        list.append(current_node.value)
-        if current_node.next is not None:
-            current_node = current_node.next
-        else:
-            break
+    print("Original Linked List:")
+    print_linked_list(head)
 
-    print(list)
+    # Create an instance of Solution and reverse the linked list
+    solution = Solution()
+    reversed_head = solution.reverseList(head)
 
-
-if __name__ == '__main__':
-    node = LinkedList(0)
-    node2 = LinkedList(1)
-    node.next = node2
-    node3 = LinkedList(2)
-    node2.next = node3
-    node4 = LinkedList(3)
-    node3.next = node4
-    node5 = LinkedList(4)
-    node4.next = node5
-    node6 = LinkedList(5)
-    node5.next = node6
-    node7 = LinkedList(6)
-    node6.next = node7
-    node8 = LinkedList(7)
-    node7.next = node8
-    node9 = LinkedList(8)
-    node8.next = node9
-    node10 = LinkedList(9)
-    node9.next = node10
-    # list = reverse_linked_list(node)
-    list2 = reverse_linked_list2(node)
-    # print_linked_list(list)
-    print_linked_list(list2)
+    print("Reversed Linked List:")
+    print_linked_list(reversed_head)
